f8b9cd6477b5539353a95af05fcf428cf5b50135
MarijnSt <marijn.stammeleer@gmail.com>
2021-04-10 23:58:05 +0200

    added user routes and empty controller

--- api/routes/api.php
+++ api/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,3 +26,14 @@ Route::post('/logout', [LoginController::class, 'logout']);
 
 
 // AUTHENTICATED ROUTES
+Route::middleware('auth:sanctum')->group(function (){
+    Route::resource('users', UserController::class);
+    // METHOD      URI                     NAME            CONTROLLER ACTION
+    // GET|HEAD    api/users               users.index     UserController@index
+    // POST        api/users               users.store     UserController@store
+    // GET|HEAD    api/users/create        users.create    UserController@create
+    // GET|HEAD    api/users/{user}        users.show      UserController@show
+    // PUT|PATCH   api/users/{user}        users.update    UserController@update
+    // DELETE      api/users/{user}        users.destroy   UserController@destroy
+    // GET|HEAD    api/users/{user}/edit   users.edit      UserController@edit
+});
